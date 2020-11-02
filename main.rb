0aa633ba2b578188fd804e4e07f798f3871a3117
Div685 <divyeshpatel685@gmail.com>
2020-11-02 13:08:23 +0300

    Add bubble_sor_by method

--- main.rb
+++ main.rb
@@ -13,3 +13,21 @@ def bubble_sort(array)
   end
   array
 end
+
+
+def bubble_sort_by(array)
+  loop do
+    flag = false
+
+    (array.size-1).times do |index|
+      if yield(array[index], array[index + 1]).positive?
+        array[index], array[index +1] = array[index+1], array[index]
+        flag = true
+      end
+    end
+
+    break unless flag
+
+  end
+  array
+end
